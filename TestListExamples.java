713121b2674cd81b7454e416f13f9c6319f9937c
Ragalina Palaka <ragalinapalaka@gmail.com>
2022-11-15 09:41:52 -0800

    lab 7

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,45 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+class checkA {
+  public boolean checkString(String s) implements StringChecker {
+      for (char c : s.toCharArray()) {
+          if (c == 'a') {return true;}
+      }
+      return false;
+  }
+}
 
 public class TestListExamples {
   // Write your grading tests here!
+
+  @Test
+  public void testSort() {
+    List<String> a = Arrays.asList("a", "c", "d");
+    /*
+    a.add("a");
+    a.add("c");
+    a.add("d");
+    */
+    List<String> b = Arrays.asList("b", "e");
+    /*
+    b.add("b");
+    b.add("e");
+    */
+    List<String> result = ListExamples.merge(a, b);
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "e");
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void testFilter() {
+    checkA sc = new checkA(); 
+    List<String> og = Arrays.asList("a", "b", "c", "d", "e");
+    List<String> result = ListExamples.filter(og, sc);
+    List<String> expected = Arrays.asList("a");
+    assertEquals(expected, result);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -2,6 +2,39 @@
 
 set -e
 
+CPATH="student-submission/ListExamples.java"
+JUNITPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+
 rm -rf student-submission
 git clone $1 student-submission
+
+if [[ ! -f $CPATH ]]
+then
+    echo "ListExamples.java wasn't found :("
+    exit
+fi
+
+cp $CPATH ../list-examples-grader
+
+set +e 
+
+javac ListExamples.java TestListExamples.java 2> output.txt
+java ListExamples TestListExamples 
+
+if [[ ! $? = 0 ]]
+then
+    echo "ListExamples.java failed to compile :/"
+    cat output.txt
+    exit
+fi
+
+javac -cp $JUNITPATH TestListExamples.java 2> junitout.txt
+java -cp $JUNITPATH org.junit.runner.JUnitCore TestListExamples 2> junitout.txt
+
+if [[ ! $? = 0 ]]
+then
+    echo "There were issues with the junit tests :,("
+    cat junitout.txt
+    exit
+fi
 
